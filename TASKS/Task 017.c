940451274d711795e39bfd374e9d122758181f1c
seurcoding <mbiseg108@gmail.com>
2023-02-01 08:08:16 -0800

    i add task 17 answers

--- TASKS/Task 017.c
+++ TASKS/Task 017.c
@@ -9,3 +9,26 @@
     Output:
     21      // That is; 1+2+3+4+5+6 = 21
 */
+
+#include <stdio.h>
+
+int sum_of_numbers(int n)
+{
+    if (n <= 0) {
+        return 0;
+    } else
+     {
+        return n + sum_of_numbers(n-1);
+    }
+}
+
+int main() {
+    int num, result;
+
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+    result = sum_of_numbers(num);
+    printf("Sum of numbers from 1 to %d: %d\n", num, result);
+    return 0;
+}
+
