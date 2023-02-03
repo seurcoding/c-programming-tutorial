73c46468cb594ed19736c1998b9a211b28a7700a
seurcoding <mbiseg108@gmail.com>
2023-02-02 19:12:57 -0800

    add answer to 19

--- TASKS/Task 019.c
+++ TASKS/Task 019.c
@@ -1,4 +1,37 @@
 /*
   Write a function to convert temperature from Celsius to
-  Fahrenheit and vice versa.  
+  Fahrenheit and vice versa.
 */
+
+#include<stdio.h>
+
+float covces(float t)
+{
+    return ((t*9/5)+32);
+}
+float covfeh(float t)
+{
+    return (((t-32)*5)/9);
+}
+int main()
+{
+  float t1, t2,c,f;
+
+  printf("Enter Temperature in Celsius : ");
+  scanf("%f",&t1);
+
+  printf("\nEnter Temperature in Fahrenheit : ");
+  scanf("%f",&t2);
+
+  //Calling Function to convert Temperature From Celsius To Fahrenheit
+  f = covces(t1);
+
+  //Calling Function to convert Temperature From Fahrenheit To Celsius
+  c = covfeh(t2);
+
+  //Display Result
+  printf("\nTemperature in Fahrenheit : %f",f);
+  printf("\nTemperature in Celsius  : %f",c);
+
+  return 0;
+}
