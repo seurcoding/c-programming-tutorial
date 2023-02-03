e093637e754ff6d95262f4a6b18151edf990af81
seurcoding <mbiseg108@gmail.com>
2023-02-02 19:33:12 -0800

    task 01 in function.c

---
+++ TASKS/sum.c
@@ -0,0 +1,20 @@
+/*Write a function to find the sum of two numbers*/
+
+#include <stdio.h>
+
+int add(int a, int b)
+{
+    int result;
+    result=a+b;
+    {
+        printf("%d and %d = %d",a,b,result);
+    }
+}
+int main() 
+{
+ add(3,5);
+
+return 0;
+}
+
+/* my function is receiving two arg which are number to be added*/
